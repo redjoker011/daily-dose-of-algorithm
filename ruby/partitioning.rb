cd9edcdd197e4aed14adcef115a73eed854fa396
Peter John Alvarado <peterjohn@gorated.ph>
2020-12-10 00:38:00 +0800

    [feat] Rewrite Partitioning Add more comments to help easily understand the algorithm

--- ruby/partitioning.rb
+++ ruby/partitioning.rb
@@ -1,43 +1,48 @@
-require_relative './array_randomizer.rb'
+require_relative 'array_randomizer'
 
 class Partitioning
-  attr_accessor :randomized_array
+  attr_reader :randomized_array
+
   def initialize
-    arr = ArrayRandomizer.new(10)
-    @randomized_array = arr.randomize
+    @randomized_array = ArrayRandomizer.new(10).randomize
   end
 
+  # Partition Collection by moving highest value from the left to the lowest
+  # value in the right based on pivot
   def partition(pivot)
     left_pointer = -1
     right_pointer = randomized_array.length
 
     loop do
-      puts "\n\n #{randomized_array} \n\n"
-      while left_pointer < (randomized_array.length - 1) && (randomized_array[left_pointer += 1]) < pivot; end
+      # Iterate over randomized_array while incrementing left_pointer until we
+      # find a larger value than our pivot
+      # We use `randomized_array.length - 1` since we start the left_pointer with - 1 and we
+      # don't want to exceed iteration over array length since were accessing
+      # elements by index i.e array length != index
+      # index starts with 0
+      # array length is the actual element count e.g [1,2,3] -> length is 3
+      while left_pointer <= (randomized_array.length - 1) && randomized_array[left_pointer += 1] < pivot; end
+      puts "Left Pointer Set to #{randomized_array[left_pointer]} \n"
 
-      puts "Left Pointer #{randomized_array[left_pointer]} in index #{left_pointer} is greater than pivot #{pivot} \n"
-
+      # Iterate over randomized_array while decrementing right_pointer until we
+      # find a smaller value than our pivot
       while right_pointer > 0 && randomized_array[right_pointer -= 1] > pivot; end
+      puts "Right Pointer Set to #{randomized_array[right_pointer]} \n"
 
-      puts "Right Pointer #{randomized_array[right_pointer]} in index #{right_pointer} is less than pivot #{pivot} \n"
+      # Break out of the loop if we already iterate all the elements
+      break if left_pointer >= right_pointer
 
-      if left_pointer >= right_pointer
-        puts "breaking out of the loop; Left Pointer: #{left_pointer} Right Pointer: #{right_pointer} \n"
-        puts "Partitioned Array #{randomized_array}"
-        break
-      end
-
-      swap(left_pointer, right_pointer)
+      puts "Swapping Values \n"
+      swap_values(left_pointer, right_pointer)
     end
+
+    randomized_array
   end
 
-  def swap(left, right)
-    puts "Swapping Values \n"
-    puts "#{randomized_array} \n"
-    puts "Swapping Values for Left: #{randomized_array[left]} Right: #{randomized_array[right]} \n"
-    left_val = randomized_array[left]
-    randomized_array[left] = randomized_array[right]
-    randomized_array[right] = left_val
-    puts "Swapping Done! \n"
+  # Swap values from left pointer to right pointer using index
+  def swap_values(left_pointer, right_pointer)
+    left_tmp = randomized_array[left_pointer]
+    randomized_array[left_pointer] = randomized_array[right_pointer]
+    randomized_array[right_pointer] = left_tmp
   end
 end

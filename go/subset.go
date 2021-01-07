91aad59907a852757a4b93435237fd12bd1a962e
Peter John Alvarado <peterjohn@gorated.ph>
2021-01-07 23:18:37 +0800

    [feat] Add helpful comment for recursion with set generation

--- go/subset.go
+++ go/subset.go
@@ -15,6 +15,21 @@ func main() {
 
 // Generate Subset recursively
 // https://stackoverflow.com/a/56925348/8680724
+// search(0);
+// -> search(1); // with 0 not in
+// ->-> search(2); // with 0 not in AND 1 in
+// ->->-> search (3); // with 0 not in AND 1 in AND 2 in. terminates with  (1,2)
+// ->->-> search (3); // with 0 not in AND 1 in AND 2 not in. terminates with  (1)
+// ->-> search(2); // with 0 not in AND 1 not in
+// ->->-> search (3); // with 0 not in AND 1 not in AND 2 in. terminates with  (2)
+// ->->-> search (3); // with 0 not in AND 1 not in AND 2 not in. terminates with  ()
+// -> search(1); // with 0 in
+// ->-> search(2); // with 0 in AND 1 in
+// ->->-> search (3); // with 0 in AND 1 in AND 2 in. terminates with (0,1,2)
+// ->->-> search (3); // with 0 in AND 1 in AND 2 not in. terminates with (0,1)
+// ->-> search(2); // with 0 in AND 1 not in
+// ->->-> search (3); // with 0 in AND 1 not in AND 2 in. terminates with  (0,2)
+// ->->-> search (3); // with 0 in AND 1 not in AND 2 not in. terminates with  (0)
 func search(k int) {
 	if k == n {
 		// Process Subsets
